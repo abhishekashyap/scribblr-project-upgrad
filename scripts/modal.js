21bc2c347d47581c1debb7623d7cb70767d34ce1
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-03-26 01:18:59 +0530

    Created sign up and sign in modals

---
+++ scripts/modal.js
@@ -0,0 +1,47 @@
+const signUpModal = document.getElementById("signup-modal");
+const signInModal = document.getElementById("signin-modal");
+const signInBtn = document.getElementById("signin");
+const signUpBtn = document.getElementById("signup");
+const signUpSwitchBtn = document.getElementById("sign-up-switch");
+const closeSignUpModalBtn = document.getElementsByClassName("close")[0];
+const closeSignInModalBtn = document.getElementsByClassName("close")[1];
+
+function closeModal(modal) {
+  modal.style.display = "none";
+}
+
+function openModal(modal) {
+  modal.style.display = "block";
+}
+
+signInBtn.onclick = function () {
+  openModal(signInModal);
+};
+
+signUpBtn.onclick = function () {
+  openModal(signUpModal);
+};
+
+signUpSwitchBtn.onclick = function () {
+  closeModal(signInModal);
+  openModal(signUpModal);
+};
+
+// X btn to close the modal
+closeSignUpModalBtn.onclick = function () {
+  closeModal(signUpModal);
+};
+
+closeSignInModalBtn.onclick = function () {
+  closeModal(signInModal);
+};
+
+// Handle outside modal click
+window.onclick = function (event) {
+  if (event.target == signUpModal) {
+    closeModal(signUpModal);
+  }
+  if (event.target == signInModal) {
+    closeModal(signInModal);
+  }
+};
